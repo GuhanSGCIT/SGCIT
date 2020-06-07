4afca5419233924395d5faa0cb298e2522560990
Guhan S <54535771+GuhanSGCIT@users.noreply.github.com>
2020-06-07 13:05:53 +0530

    Add files via upload

---
+++ floating point number with 1 decimal place round it off to nearest greater integer and print it.py
@@ -0,0 +1,2 @@
+from random import choice
+print(choice([round(float(input()))+1,round(float(input()))]))
---
+++ the nth number of series..py
@@ -0,0 +1,9 @@
+def nthTerm(n) : 
+  
+    return 2 * pow(n, 3) + pow(n, 2) 
+  
+# Driver code 
+if __name__ == "__main__" : 
+  
+    N = int(input())
+    print(nthTerm(N)) 
